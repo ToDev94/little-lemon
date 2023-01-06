0e88f178aef494cc1b87254cd0de13c3d265e61d
“ToDev94” <nefoucitoufik@gmail.com>
2023-01-06 14:35:04 +0100

    Added Open Graph Protocol Meta tags

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,55 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <img
+        src="./restauranfood.jpg"
+        alt="restaurant"
+        width="173px"
+        height="274.22px"
+      />
+      <div>
+        <h3>Navigation</h3>
+        <ul>
+          <li>
+            <a href="./"> Home </a>
+          </li>
+          <li>
+            <a href="./about"> About </a>
+          </li>
+          <li>
+            <a href="./menu"> Menu </a>
+          </li>
+          <li>
+            <a href="./reservations"> Reservations </a>
+          </li>
+          <li>
+            <a href="./order"> Order Online </a>
+          </li>
+          <li>
+            <a href="./login"> Login </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3>Contact</h3>
+        <ul>
+          <li> Address </li>
+          <li> Phone number </li>
+          <li> Email </li>
+        </ul>
+      </div>
+      <div>
+        <h3>Social Media Links</h3>
+        <ul>
+          <li> Address </li>
+          <li> Phone number </li>
+          <li> Email </li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
