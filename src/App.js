3bd4bfad0e8e525ee17c06f8cd405b0a08372c5a
“ToDev94” <nefoucitoufik@gmail.com>
2023-01-06 11:50:02 +0100

    Adding semantic tags

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import Header from "./Header";
+
+import Main from "./Main";
+import Footer from "./Footer";
 
 function App() {
-  return <div className="App">Home page</div>;
+  return (
+    <>
+      <Header />
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <header>
+      <img src="./Logo.svg" alt="logo" />
+      <Nav />
+    </header>
+  );
+};
+
+export default Header;
